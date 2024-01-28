ce53f130aa8f1377569d00db8a59c115fc9c9a00
cvams <cvamsifeb21@gmail.com>
2024-01-28 15:23:18 -0500

    Fixed some bugs, WRT to previous commit, still needs clean up

--- src/main/java/com/airlineticket/App/models/User.java
+++ src/main/java/com/airlineticket/App/models/User.java
@@ -5,6 +5,7 @@ import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
+import org.antlr.v4.runtime.misc.NotNull;
 import org.springframework.security.core.GrantedAuthority;
 import org.springframework.security.core.authority.SimpleGrantedAuthority;
 import org.springframework.security.core.userdetails.UserDetails;
