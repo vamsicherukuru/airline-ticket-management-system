28c583c7e975f5ff6c562d2ef60e4b085032d87d
cvams <cvamsifeb21@gmail.com>
2024-02-04 03:50:35 -0500

    Home page search box airport retrieval

--- src/main/java/com/airlineticket/App/controller/DefaultController.java
+++ src/main/java/com/airlineticket/App/controller/DefaultController.java
@@ -1,18 +1,27 @@
 package com.airlineticket.App.controller;
 
 
+import com.airlineticket.App.models.Airport;
+import com.airlineticket.App.models.flights.FlightDetails;
+import com.airlineticket.App.repos.AirportRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+
+import java.util.List;
 
 @Controller
 public class DefaultController {
 
-
+    @Autowired
+    AirportRepository airportRepository;
     @GetMapping("/")
-    public String homePage(){
-
+    public String homePage(Model model){
+        List<Airport> airports = airportRepository.findAll();
+        model.addAttribute("airports",airports);
         return "home";
     }
     @GetMapping("/profile")
