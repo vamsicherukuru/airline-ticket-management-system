549865fadcc6617e2c40d78df6483712cab2f5a5
cvams <cvamsifeb21@gmail.com>
2024-02-04 15:42:20 -0500

    Home UI Changes

--- src/main/java/com/airlineticket/App/controller/DefaultController.java
+++ src/main/java/com/airlineticket/App/controller/DefaultController.java
@@ -3,7 +3,9 @@ package com.airlineticket.App.controller;
 
 import com.airlineticket.App.models.Airport;
 import com.airlineticket.App.models.flights.FlightDetails;
+import com.airlineticket.App.models.flights.TripDetails;
 import com.airlineticket.App.repos.AirportRepository;
+import com.airlineticket.App.repos.TripDetailsRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.context.SecurityContextHolder;
@@ -18,12 +20,23 @@ public class DefaultController {
 
     @Autowired
     AirportRepository airportRepository;
+
+    @Autowired
+    TripDetailsRepository tripDetailsRepository;
+
+
+
     @GetMapping("/")
     public String homePage(Model model){
         List<Airport> airports = airportRepository.findAll();
+        List<TripDetails> trips = tripDetailsRepository.findAll();
         model.addAttribute("airports",airports);
+        model.addAttribute("trips",trips);
         return "home";
     }
+
+
+
     @GetMapping("/profile")
     public String profilePage(){
 
@@ -38,6 +51,9 @@ public class DefaultController {
 
 
     }
+
+
+
 
     @GetMapping("/admin")
     public String adminPage(){
